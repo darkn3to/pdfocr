a2335bf4e117958d01ac48bd36927091c8f1c8a6
Dhruv <62509177+darkn3to@users.noreply.github.com>
2024-11-11 22:18:22 +0530

    *orientation done
    next: graft text.

--- app/src/main/java/org/example/app/components/pdfHandler.java
+++ app/src/main/java/org/example/app/components/pdfHandler.java
@@ -9,13 +9,14 @@ import javax.imageio.ImageIO;
 import net.sourceforge.tess4j.ITessAPI;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.Word;
-import net.sourceforge.tess4j.OSDResult;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.apache.pdfbox.rendering.PDFRenderer;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+
 import org.springframework.stereotype.Component;
 
 @Component
@@ -33,19 +34,24 @@ public class pdfHandler {
             newDoc = new PDDocument();
 
             for (int i = 0; i < doc.getNumberOfPages(); i++) {
+                PDPage page = doc.getPage(i);
                 BufferedImage image = renderer.renderImageWithDPI(i, 300);
                 String text = inst.doOCR(image);
                 List<Word> words = inst.getWords(image, ITessAPI.TessPageIteratorLevel.RIL_WORD);
+
+                PDRectangle mb = page.getMediaBox();
+                boolean isLandscape = mb.getWidth() > mb.getHeight();
+
                 Graphics2D g2d = image.createGraphics();
                 g2d.setColor(Color.RED);
                 g2d.setStroke(new BasicStroke(3));
+                g2d.setFont(new Font("Arial", Font.PLAIN, 12));
                 for (Word word : words) {
-
-                    if (word.getConfidence() > 75.0) {
-                        System.out.println("Word: " + word.getText() + ", Bounding Box: " + word.getBoundingBox() + ", Confidence: " + word.getConfidence());
+                    if (word.getConfidence() > 35.0) { // TODO: pre-process images and increase confidence threshold
+                        //System.out.println("Word: " + word.getText() + ", Bounding Box: " + word.getBoundingBox() + ", Confidence: " + word.getConfidence());
                         g2d.draw(word.getBoundingBox());
+                        g2d.drawString(String.format("%.2f", word.getConfidence()), (float) word.getBoundingBox().getX(), (float) (word.getBoundingBox().getY() + word.getBoundingBox().getHeight() + 12));
                     }
-                    g2d.drawString(String.format("%.2f", word.getConfidence()), (float) word.getBoundingBox().getX(), (float) (word.getBoundingBox().getY() + word.getBoundingBox().getHeight() + 12));
                 }
                 g2d.dispose();
 
@@ -54,6 +60,13 @@ public class pdfHandler {
                 System.out.println("Modified image saved successfully at: " + outputImageFile.getAbsolutePath());
 
                 PDPage newPage = new PDPage();
+
+                if (isLandscape == true) {
+                    newPage.setMediaBox(new PDRectangle(PDRectangle.A4.getHeight(), PDRectangle.A4.getWidth()));
+                } 
+                else {
+                    newPage.setMediaBox(PDRectangle.A4);
+                }
                 newDoc.addPage(newPage);
 
                 PDImageXObject pdImage = PDImageXObject.createFromFileByContent(outputImageFile, newDoc);
